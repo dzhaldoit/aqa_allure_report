5097d8323de40a059c9be9c0475de1f158a8a59a
Dzhalal Atabaev <djaldoit@gmail.com>
2024-10-09 15:58:07 +0300

    hw 0.1

--- src/test/java/allure/AllureStepTest.java
+++ src/test/java/allure/AllureStepTest.java
@@ -1,2 +1,67 @@
-package allure;public class AllureStepTest {
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class AllureStepTest {
+
+    private final static String REPOSITORY = "eroshenkoam/allure-example";
+    private final static String ISSUE_NAME = "e.sh";
+
+    @Test
+    public void cleanSelenideTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+        $("[data-target='qbsearch-input.inputButtonText']").click();
+        $("#query-builder-test").sendKeys(REPOSITORY);
+        $("#query-builder-test").submit();
+
+        $(linkText(REPOSITORY)).click();
+        $("#issues-tab").click();
+        $(withText(ISSUE_NAME)).should(Condition.exist);
+    }
+
+    @Test
+    public void lambdaStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу ", () -> {
+            open("https://github.com/");
+        });
+        step("Поиск репозитория " + REPOSITORY, () -> {
+            $("[data-target='qbsearch-input.inputButtonText']").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Кликаем по ссылке " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Кликаем вкладку Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие задачи " + ISSUE_NAME, () -> {
+            $(withText(ISSUE_NAME)).should(Condition.exist);
+        });
+    }
+
+    @Test
+    public void annotatedStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebStepTest steps = new WebStepTest();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithName(ISSUE_NAME);
+    }
 }
--- src/test/java/allure/WebStepTest.java
+++ src/test/java/allure/WebStepTest.java
@@ -1,2 +1,39 @@
-package allure;public class WebStep {
+package allure;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebStepTest {
+
+    @Step("Открываем главную страницу GitHub")
+    public void openMainPage() {
+        open("https://github.com/");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $("[data-target='qbsearch-input.inputButtonText']").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Нажимаем на ссылку {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(By.linkText(repo)).click();
+    }
+
+    @Step("Кликнуть на вкладку Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с названием {issueName}")
+    public void shouldSeeIssueWithName(String issueName) {
+        $(withText(issueName)).should(Condition.exist);
+    }
 }
